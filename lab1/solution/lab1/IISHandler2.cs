86500d10eee267f6245bf83d03508fb53efd1289
antony <tehanovanton@gmail.com>
2022-09-19 06:51:39 +0300

    lab1 done

---
+++ lab1/solution/lab1/IISHandler2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Web;
+
+namespace lab1
+{
+    public class IISHandler2 : IHttpHandler
+    {
+        public bool IsReusable
+        {
+            get { return true; }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            HttpResponse response = context.Response;
+            /*C: \Users\Anton\source\4k - 1c\labs\progrmming_web_services\lab1\solution\lab1\HtmlPage1.html*/
+            string htmlPage = File.ReadAllText(@"C:\Users\Anton\source\4k-1c\labs\progrmming_web_services\lab1\solution\lab1\HtmlPage1.html");
+
+            response.ContentType = "text/html";
+            response.Write(htmlPage);
+        }
+    }
+}
