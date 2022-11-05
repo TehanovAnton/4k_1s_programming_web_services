ad084c07dafc5838d4001d62454fae7be4dc3225
antony <tehanovanton@gmail.com>
2022-11-05 05:43:24 +0300

    WinForm service concat method worl through connected services

---
+++ lab4/WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.param1 = new System.Windows.Forms.TextBox();
+            this.param2 = new System.Windows.Forms.TextBox();
+            this.result = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // param1
+            // 
+            this.param1.Location = new System.Drawing.Point(170, 88);
+            this.param1.Name = "param1";
+            this.param1.Size = new System.Drawing.Size(125, 27);
+            this.param1.TabIndex = 0;
+            // 
+            // param2
+            // 
+            this.param2.Location = new System.Drawing.Point(170, 121);
+            this.param2.Name = "param2";
+            this.param2.Size = new System.Drawing.Size(125, 27);
+            this.param2.TabIndex = 1;
+            // 
+            // result
+            // 
+            this.result.Location = new System.Drawing.Point(170, 154);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(125, 27);
+            this.result.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(301, 121);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(401, 121);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "concat";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(858, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.param2);
+            this.Controls.Add(this.param1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox param1;
+        private System.Windows.Forms.TextBox param2;
+        private System.Windows.Forms.TextBox result;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ lab4/WinFormsApp1/Form1.cs
@@ -0,0 +1,51 @@
+﻿using ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static ServiceReference1.SimplexSoapClient;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private SimplexSoapClient proxyClient;
+        public Form1()
+        {
+            proxyClient = new SimplexSoapClient(SimplexSoapClient.EndpointConfiguration.SimplexSoap12, "https://localhost:44349/Simplex.asmx");
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // add
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // concat
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            string val1 = param1.Text.ToString();
+            double val2;
+
+            if (!string.IsNullOrEmpty(val1) && double.TryParse(param2.Text.ToString(), out val2))
+            {
+                result.Text = (await proxyClient.ConcatAsync(val1, val2)).sum_2Result;
+            }
+            else
+            {
+                result.Text = "Error!";
+            }
+        }
+    }
+}
