76e27aac441895d2a09e94bb6236f39426af722d
antony <tehanovanton@gmail.com>
2022-11-28 23:40:37 +0300

    sindication service work

---
+++ lab7/solution/Feed1.cs
@@ -0,0 +1,85 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Syndication;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace solution
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "Feed1" в коде и файле конфигурации.
+    public class Feed1 : IFeed1
+    {
+        public SyndicationFeedFormatter CreateFeed()
+        {
+            // Создать новый веб-канал.
+            SyndicationFeed feed = new SyndicationFeed("Feed Title", "A WCF Syndication Feed", null);
+            List<SyndicationItem> items = new List<SyndicationItem>();
+
+            // Создать новый элемент рассылки.
+            SyndicationItem item = new SyndicationItem("An item", "Item content", null);
+            items.Add(item);
+            feed.Items = items;
+
+            // Возвращать канал ATOM или RSS, основываясь на строке запроса
+            // RSS-&gt; http://localhost:8733/Design_Time_Addresses/solution/Feed1/
+            // Atom-&gt; http://localhost:8733/Design_Time_Addresses/solution/Feed1/?format=atom
+            string query = WebOperationContext.Current.IncomingRequest.UriTemplateMatch.QueryParameters["format"];
+            SyndicationFeedFormatter formatter = null;
+            if (query == "atom")
+            {
+                formatter = new Atom10FeedFormatter(feed);
+            }
+            else
+            {
+                formatter = new Rss20FeedFormatter(feed);
+            }
+
+            return formatter;
+        }
+
+        public SyndicationFeedFormatter GetStudentNotes(string studentId)
+        {
+            SyndicationFeed feed = new SyndicationFeed("Subjects & Notes", "Get list of notes by all subjects for this student", null);
+            List<SyndicationItem> items = new List<SyndicationItem>();
+
+            string wdsNotesUrl = "http://localhost:51172/DataServices/WDS.svc/Notes/";
+            string studentFilter = String.Format("StudentId%20eq%20{0}", studentId);
+            string requestUrl = String.Format("{0}?$format={1}&$filter={2}", wdsNotesUrl, "json", studentFilter);
+
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(requestUrl);
+            request.Method = "GET";
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+
+            string responseString = reader.ReadToEnd();
+
+            var notesResp = JsonConvert.DeserializeObject<NoteResponse>(responseString);
+            var notes = notesResp.Value;
+
+            foreach (var note in notes)
+            {
+                items.Add(new SyndicationItem(note.Subj, note.NoteValue.ToString(), null));
+            }
+            feed.Items = items;
+
+            string query = WebOperationContext.Current.IncomingRequest.UriTemplateMatch.QueryParameters["format"];
+            SyndicationFeedFormatter formatter;
+            if (query == "atom")
+            {
+                formatter = new Atom10FeedFormatter(feed);
+            }
+            else
+            {
+                formatter = new Rss20FeedFormatter(feed);
+            }
+            return formatter;
+        }
+    }
+}
---
+++ lab7/solution/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью указанного ниже 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// общие сведения об этой сборке.
+[assembly: AssemblyTitle("solution")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("solution")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Если для атрибута ComVisible задать значение FALSE, типы в этой сборке будут невидимыми 
+// для компонентов COM. Если в этой сборке необходимо получить доступ к типу из 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий идентификатор GUID представляет идентификатор typelib, если этот проект доступен через COM
+[assembly: Guid("01bf3a68-ef49-4859-a7a5-86a0eec7982c")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
