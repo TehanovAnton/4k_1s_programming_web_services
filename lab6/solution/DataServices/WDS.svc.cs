37e9c10774fe2a489464e68d2a946d7a2cf1c054
antony <tehanovanton@gmail.com>
2022-11-28 21:08:23 +0300

    lab6 done

--- lab6/solution/DataServices/WDS.svc.cs
+++ lab6/solution/DataServices/WDS.svc.cs
@@ -3,6 +3,7 @@
 //     Copyright (c) Microsoft Corporation.  All rights reserved.
 // </copyright>
 //------------------------------------------------------------------------------
+
 using solution.EDMModels;
 using System;
 using System.Collections.Generic;
@@ -10,11 +11,13 @@ using System.Data.Services;
 using System.Data.Services.Common;
 using System.Data.Services.Providers;
 using System.Linq;
+using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Web;
 
 namespace solution.DataServices
 {
+    [ServiceBehavior(IncludeExceptionDetailInFaults = true)]
     public class WDS : EntityFrameworkDataService<WDSEntities>
     {
         // This method is called only once to initialize service-wide policies.
@@ -22,9 +25,58 @@ namespace solution.DataServices
         {
             // TODO: set rules to indicate which entity sets and service operations are visible, updatable, etc.
             // Examples:
-            config.SetEntitySetAccessRule("*", EntitySetRights.AllRead);
-            // config.SetServiceOperationAccessRule("MyServiceOperation", ServiceOperationRights.All);
+            config.SetEntitySetAccessRule("*", EntitySetRights.All);
+            config.SetServiceOperationAccessRule("*", ServiceOperationRights.All);
             config.DataServiceBehavior.MaxProtocolVersion = DataServiceProtocolVersion.V3;
+        }
+
+        [WebGet]
+        public IQueryable<Students> InsertStudent(string name)
+        {
+            Students student = new Students();
+            student.Name = name;
+
+            WDSEntities context = this.CurrentDataSource;
+            context.Students.Add(student);
+            context.SaveChanges();
+
+            return context.Students;
+        }
+
+        [WebGet]
+        public IQueryable<Notes> InsertNote(string subj, int noteValue, int studentId)
+        {
+            Notes note = new Notes();
+            note.Subj = subj;
+            note.StudentId = studentId;
+            note.NoteValue = noteValue;
+
+            WDSEntities context = this.CurrentDataSource;
+            context.Notes.Add(note);
+            context.SaveChanges();
+
+            return context.Notes;
+        }
+
+        [WebGet]
+        public IQueryable<Students> ChangeStudent(int id, string name)
+        {
+            WDSEntities context = this.CurrentDataSource;
+            Students student = context.Students.Find(id);
+            student.Name = name;
+            context.SaveChanges();
+            return context.Students;
+        }
+
+        [WebGet]
+        public IQueryable<Notes> ChangeNote(int id, string subj, int noteValue)
+        {
+            WDSEntities context = this.CurrentDataSource;
+            Notes note = context.Notes.Find(id);
+            note.Subj = subj;
+            note.NoteValue = noteValue;
+            context.SaveChanges();
+            return context.Notes;
         }
     }
 }
